641c5d3b73ba070e25248f27ec71175b8f6a5341
Jesus Hernandez <jesushdez@protonmail.com>
2020-11-20 05:06:38 +0100

    refactor: Use AsRef<str> to make API more ergonomic (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,20 @@ pub use error::DropRootError;
 
 use std::ffi::CString;
 
-fn get_group_id_of(group_name: &str) -> Result<libc::gid_t, DropRootError> {
-    let group_record = unsafe { libc::getgrnam(CString::new(group_name)?.as_ptr()) };
+fn get_group_id_of<T: AsRef<str>>(group_name: T) -> Result<libc::gid_t, DropRootError> {
+    let group_record = unsafe { libc::getgrnam(CString::new(group_name.as_ref())?.as_ptr()) };
     if group_record.is_null() {
-        log::error!("Unable to getgrnam of the group {}", group_name);
+        log::error!("Unable to getgrnam of the group {}", group_name.as_ref());
         return Err(DropRootError::last_os_error());
     }
     let group_id = unsafe { (*group_record).gr_gid };
     return Ok(group_id);
 }
 
-fn get_user_id_of(user_name: &str) -> Result<libc::gid_t, DropRootError> {
-    let passwd_record = unsafe { libc::getpwnam(CString::new(user_name)?.as_ptr()) };
+fn get_user_id_of<T: AsRef<str>>(user_name: T) -> Result<libc::gid_t, DropRootError> {
+    let passwd_record = unsafe { libc::getpwnam(CString::new(user_name.as_ref())?.as_ptr()) };
     if passwd_record.is_null() {
-        log::error!("Unable to getpwnam of the user {}", user_name);
+        log::error!("Unable to getpwnam of the user {}", user_name.as_ref());
         return Err(DropRootError::last_os_error());
     }
     let user_id = unsafe { (*passwd_record).pw_uid };
@@ -29,10 +29,10 @@ fn get_user_id_of(user_name: &str) -> Result<libc::gid_t, DropRootError> {
 /// Set current process group
 /// 
 /// This can drop privilegies of current process.
-pub fn set_group(group_name: &str) -> Result<(), DropRootError> {
+pub fn set_group<T: AsRef<str>>(group_name: T) -> Result<(), DropRootError> {
     let group_id = get_group_id_of(group_name)?;
     if unsafe { libc::setgid(group_id) } != 0 {
-        log::error!("Unable to setgid of group {}", group_name);
+        log::error!("Unable to setgid of group {}", group_name.as_ref());
         return Err(DropRootError::last_os_error());
     }
     return Ok(());
@@ -41,10 +41,10 @@ pub fn set_group(group_name: &str) -> Result<(), DropRootError> {
 /// Set the current process user
 /// 
 /// This can drop privilegies of current process.
-pub fn set_user(user_name: &str) -> Result<(), DropRootError> {
+pub fn set_user<T: AsRef<str>>(user_name: T) -> Result<(), DropRootError> {
     let user_id = get_user_id_of(user_name)?;
     if unsafe { libc::setuid(user_id) } != 0 {
-        log::error!("Unable to setuid of user {}", user_name);
+        log::error!("Unable to setuid of user {}", user_name.as_ref());
         return Err(DropRootError::last_os_error());
     }
     return Ok(());
@@ -53,9 +53,9 @@ pub fn set_user(user_name: &str) -> Result<(), DropRootError> {
 /// Set current process user and group
 /// 
 /// This can drop privilegies of current process.
-pub fn set_user_group(user: &str, group: &str) -> Result<(), DropRootError> {
+pub fn set_user_group<T: AsRef<str>>(user: T, group: T) -> Result<(), DropRootError> {
     set_group(group)?;
     set_user(user)?;
-    log::info!("Process privilages changed to {}:{}", user, group);
+    log::info!("Process privilages changed to {}:{}", user.as_ref(), group.as_ref());
     return Ok(());
 }
