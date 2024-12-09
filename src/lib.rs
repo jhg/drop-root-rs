724ae5293f16ac19f5902b6eca468a36f16cfdc2
Jesus Hernandez <jesushdez@protonmail.com>
2024-12-09 19:56:30 +0100

    Ignore tests that require root and split test in two tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,28 +19,31 @@ mod tests {
     use crate::*;
     use std::process::Command;
 
+    #[ignore]
     #[test]
-    fn is_root() {
+    fn user_and_group_does_not_exist() {
         assert_eq!(
             format!("{}", set_user_group("thisusershouldnotexistbecausenoonecallslikethis", "thisgroupshouldnotexistbecausenoonecallslikethis").unwrap_err()),
             "Bad user or group."
         );
+    }
 
-        if unsafe { libc::getuid() } != 0 {
-            assert_eq!(
-                format!("{}", set_user_group("root", "root").unwrap_err()),
-                "Operation not permitted (os error 1)"
-            );
-        } else {
-            set_user_group("nobody", "nogroup").unwrap();
-            let output = Command::new("id")
-                .output()
-                .expect("failed to execute process");
+    #[ignore]
+    #[test]
+    fn change_to_nobody_and_nogroup() {
+        set_user_group("nobody", "nogroup").unwrap();
+        let output = Command::new("id")
+            .output()
+            .expect("failed to execute process");
 
-            assert_eq!(
-                String::from_utf8_lossy(&output.stdout).trim(),
-                String::from("uid=65534(nobody) gid=65534(nogroup) groupes=65534(nogroup)")
-            );
-        }
+        assert_eq!(
+            String::from_utf8_lossy(&output.stdout).trim(),
+            String::from("uid=65534(nobody) gid=65534(nogroup) groupes=65534(nogroup)")
+        );
+
+        assert_eq!(
+            format!("{}", set_user_group("root", "root").unwrap_err()),
+            "Operation not permitted (os error 1)"
+        );
     }
 }
